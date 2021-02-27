086a58a776e9761c24d0d79270e691d6b1994728
shahid-waris <comsian.lahor@gmail.com>
2021-02-27 23:29:54 +0500

    tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// import "javascript/stylesheets/application"
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// Tailwind CSS
+ 
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+// postcss.config.js
+let environment = {
   plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -9,4 +11,22 @@ module.exports = {
       stage: 3
     })
   ]
+};
+
+// Add everything below!
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
+
+module.exports = environment;
