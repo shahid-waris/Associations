8e2ed83e3091d6cb01127594b7ac65e2a8b312a0
shahid-waris <comsian.lahor@gmail.com>
2021-02-28 11:19:15 +0500

    create users controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,9 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-// import "javascript/stylesheets/application"
+// app/javascript/packs/application.js
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 // Tailwind CSS
- 
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// import "javascript/stylesheets/application" // ADD THIS LINE
